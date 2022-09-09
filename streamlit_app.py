286e3eb3b2272327fb1817ad6ba5bb1d3a08fe8b
El Sonico <113154391+ces-git57@users.noreply.github.com>
2022-09-09 10:47:44 -0500

    Update streamlit_app.py
    Cambio de menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,8 +1,9 @@
 
 import streamlit 
-streamlit.title('My parents now healthy dinner')
+streamlit.title('My mom´s new healthy dinner')
 
-streamlit.header('Breakfast Menu')
-streamlit.text('Omega 3 & Blueberry Oatmeal')
-streamlit.text('Kale, Spinach & Rocket Smoothie')
-streamlit.text('Hard-Boiled Free-Range Egg')
+streamlit.header('Breakfast Favorite')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
